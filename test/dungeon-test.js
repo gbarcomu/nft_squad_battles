3462118f7dc9b6a43a82ec640d2eae59a1b1ebb7
gbarcomu <gbarcomu@gmail.com>
2021-12-22 01:55:37 +0100

    testing battle

--- test/dungeon-test.js
+++ test/dungeon-test.js
@@ -3,7 +3,7 @@ const { expect } = require("chai");
 describe("Dungeon", function () {
 
   let SquadNFT, squadNFT, owner, usr1, Dungeon, dungeon;
-  const validSquad = "0x0102000102111111";
+  const validSquad = "0x0001010202111111";
   const validPrice = "5.0";
 
   const byteEnemySquad = "00010200";
@@ -59,6 +59,35 @@ describe("Dungeon", function () {
       await dungeon.playQuest(0,1,2);
       await expect(dungeon.resolveQuest(`0x${byteEnemySquad}`, `0x${wrongFactor}`))
       .to.be.revertedWith("Error, quest squad is different from the commited");
+    });
+
+  });
+
+  // For now this remains as a manual check
+  describe('Quest Execution', () => {
+
+    it("Quest Execution", async function () {
+
+      const enemySquad1 = "00000000";
+
+      const commitment = ethers.utils.keccak256(`${owner.address}${enemySquad1}${nonce}${blindingFactor}`);
+      await dungeon.createQuest(commitment);
+      await dungeon.playQuest(0,1,3);
+      const tx = await dungeon.resolveQuest(`0x${enemySquad1}`, `0x${blindingFactor}`);
+      const receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+
+      console.log(`Player Squad: 0 Swordsman, 1 Lancer, 2 Knight`);
+      console.log(`Enemy Squad: 0 Swordsman, 1 Swordsman, 1 Swordsman`);
+
+      const rand1 = parseInt(receipt.logs[0].data.slice(64, 66),16);
+      const rand2 = parseInt(receipt.logs[0].data.slice(128, 130),16);
+      const rand3 = parseInt(receipt.logs[0].data.slice(192, 194),16);
+      const winner1 = receipt.logs[1].data.slice(64, 66);
+      const winner2 = receipt.logs[1].data.slice(128, 130);
+      const winner3 = receipt.logs[1].data.slice(192, 194);
+
+      console.log(`Numbers: ${rand1}, ${rand2}, ${rand3}`);
+      console.log(`Winner: ${winner1}, ${winner2}, ${winner3}`);
     });
 
   });
